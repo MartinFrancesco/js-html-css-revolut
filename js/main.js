ba0b80a7478d7ea50e3ad0d9df7fc434afe8a6f6
Francesco <francescomartinmail@gmail.com>
2020-10-28 17:32:35 +0100

    First drop

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+$( document ).ready(function() {
 
+
+//Ref
+var dropLinks = $('.with-dropdown > a');
+var dropMenu = $('.with-dropdown .dropdown-menu');
+
+
+   //EVENTI Show-Hide Menu
+   dropLinks.click( function() {
+     $(this).next(dropMenu).toggle();
+   });
+
+});
