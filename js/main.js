7b51f021e1d1df01ec48f50d08832c7f03291b33
Francesco <francescomartinmail@gmail.com>
2020-10-28 17:48:27 +0100

    Second drop

--- js/main.js
+++ js/main.js
@@ -6,9 +6,11 @@ var dropLinks = $('.with-dropdown > a');
 var dropMenu = $('.with-dropdown .dropdown-menu');
 
 
-   //EVENTI Show-Hide Menu
+   //EVENTI Show-Hide first drop
    dropLinks.click( function() {
      $(this).next(dropMenu).toggle();
    });
+
+   
 
 });
